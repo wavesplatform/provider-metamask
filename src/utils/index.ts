da9321fb37f90fcca58cc0000ec421845ec1f18f
inazarov <inazarov@web3tech.ru>
2021-10-04 18:15:02 +0300

    convert base64, base58 to hexstring

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,36 @@
-import { base58Decode } from '@waves/ts-lib-crypto';
+import { InvokeScriptCallArgument, Long } from '@waves/ts-types';
+import { base58Decode, base64Decode, base58Encode } from '@waves/ts-lib-crypto';
 import { bytesToHex } from 'node-forge/lib/util';
 import { AddEthereumChainParameter } from '../Metamask.interface';
 
-const getInvokeArgValue = (item: any) => {
-    if(item.type === 'list') {
-        return item.value.map(getInvokeArgValue);
-    } else {
-        return item.value;
+const bytesToHexString = (bytes: Uint8Array) => {
+    const hex = bytesToHex(bytes);
+
+    return `0x${hex}`;
+}
+
+const getInvokeArgValue = (item: InvokeScriptCallArgument<Long>): any => {
+    switch (item.type) {
+        case 'list':
+            return item.value.map(getInvokeArgValue);
+        case 'binary':
+            if (item.value.indexOf('base64:') === 0) {
+                const value = item.value.slice(6);
+
+                return bytesToHexString(base64Decode(value));
+            } else if (item.value.indexOf('base58:') === 0) {
+                const value = item.value.slice(6);
+
+                return bytesToHexString(base58Decode(value));
+            } else {
+                return item.value;
+            }
+        default:
+            return item.value;
     }
 };
 
-export const getInvokeArgsValues = (args: any[]): any[] => {
+export const getInvokeArgsValues = (args: InvokeScriptCallArgument<Long>[]): any[] => {
     return args.map((item) => {
         return getInvokeArgValue(item);
     });
@@ -24,8 +44,9 @@ const formatPayment = (payment: any): any[] => {
     if (assetId === 'WAVES') { // todo WAVES
         assetId = WAVES_ASSET_ID_CONVERTED;
     } else {
-        const hex = bytesToHex(base58Decode(assetId));;
-        assetId = `0x${hex}`;
+        const bytes = base58Decode(assetId);
+
+        assetId = bytesToHexString(bytes);
     }
 
     return [ assetId, amount ];
