2a36e760d06082af4f4db11b0f6e65cfe4f7b600
inazarov <inazarov@web3tech.ru>
2021-10-19 10:09:09 +0300

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,33 @@
 const webpack = require("webpack");
 const path = require('path');
 const resolve = path.resolve;
-
-const time = new Date().toLocaleDateString('ru', { hour: 'numeric', minute: 'numeric', second: 'numeric' });
-const definePLugin = new webpack.DefinePlugin({
-    VERSION: time,
-});
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = [
     {
-        entry: './src/index.ts',
         mode: "production",
-        module: {
-            rules: [
-                {
-                    test: /\.ts/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.less$/,
-                    use: [
-                        // conf.mode === 'production' ? MiniCssExtractPlugin.loader : 
-                        { loader: "style-loader" },
-                        {
-                            loader: "css-loader",
-                            options: {
-                                modules: true,
-                                localIdentName: '[folder]__[local]--[hash:base64:5]',
-                            }
-                        },
-                        {
-                            loader: "less-loader",
-                            options: { root: path.resolve(__dirname, './') }
-                        },
-                    ]
-                },
-            ],
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-        },
+        entry: './src/index.ts',
         output: {
             libraryTarget: 'umd',
             globalObject: 'this',
             library: 'providerMetamask',
             filename: 'provider-metamask.js',
             path: resolve(__dirname, 'dist'),
-        }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        // plugins: [
+        //     new BundleAnalyzerPlugin()
+        // ]
     }
 ];
